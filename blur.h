d69ee3262bdcb16c29d2d1d95f25e445475758fe
Lescurel <louis.klein7@gmail.com>
2017-02-24 15:42:46 +0100

    Removing useless define

--- blur.h
+++ blur.h
@@ -1,22 +1,12 @@
 #include <math.h>
 
-#define max(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a > _b ? _a : _b; })
-
-#define min(a,b) \
- ({ __typeof__ (a) _a = (a); \
-     __typeof__ (b) _b = (b); \
-   _a < _b ? _a : _b; })
-
 #define CHANNEL 4
 #define BLUR 20
 
 int * sizes;
 int * pixel_val;
 
-void blur_file();
+void gauss_blur();
 void box_gauss(double sigma, int n);
 void box_blur(png_bytep* scl, png_bytep* tcl, int r);
 void box_blur_h(png_bytep* scl, png_bytep* tcl,int r);
